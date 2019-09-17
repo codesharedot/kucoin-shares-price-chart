a2e4e084b7c8a628a376b4719008468bc6cec9b7
codesharedot <codeto@sent.com>
2019-09-17 17:56:34 +0200

    first commit

--- example.py
+++ example.py
@@ -10,7 +10,7 @@ from time import gmtime, strftime
 import matplotlib.pyplot as plt
 
 enddate = strftime("%Y%m%d", gmtime())
-r  = requests.get("https://coinmarketcap.com/currencies/zcash/historical-data/?start=20140101&end={0}".format(enddate))
+r  = requests.get("https://coinmarketcap.com/currencies/dogecoin/historical-data/?start=20140101&end={0}".format(enddate))
 data = r.text
 
 soup = BeautifulSoup(data, "html.parser")
@@ -44,7 +44,7 @@ print(x)
 
 x = list(range(0, len(prices)))
 
-plt.title('zcash price from 2014')
+plt.title('dogecoin price from 2014')
 plt.ylabel('Price in USD')
 plt.xlabel('Years from 2014')
 plt.bar(x, prices)
